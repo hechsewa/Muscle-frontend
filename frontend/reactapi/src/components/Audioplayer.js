b5fc1b5d208729b7ec6c415b4b5a80e625602733
hechsewa <ewahechsman@gmail.com>
2019-09-27 15:39:23 +0200

    audio player working

--- frontend/reactapi/src/components/Audioplayer.js
+++ frontend/reactapi/src/components/Audioplayer.js
@@ -1,40 +1,37 @@
 import React from "react"
+import sound from "./01 All Of Me.mp3"
 
 class Audioplayer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             playing: false,
+            src: sound,
+            cover: "https://cdn.shopify.com/s/files/1/0812/1837/products/0114-0008_grande.jpeg",
+            title: "All of me",
+            band: "Dixie Band",
+            genre: "jazz",
+            recommended: "nie"
         };
-        this.PlayAudio = this.PlayAudio.bind();
-    };
-
-    PlayAudio(){
-        function handleClick(e) {
-            e.preventDefault(); //nie można zwrócić false w celu zapobiegnięcia wykonania domyślnej akcji. Należy jawnie wywołać preventDefault
-            console.log("Klik play");
-            this.setState({ playing: true });
-            this.audio.play();
-        }
     };
 
     render() {
         return (
            <div className="audio-container">
                <div className="coverPhoto">
-                   <img src="https://cdn.shopify.com/s/files/1/0812/1837/products/0114-0008_grande.jpeg" alt="CD cover"/>
+                   <img src={this.state.cover} alt="CD cover"/>
                </div>
                <div className="metadata">
                    <ul>
-                       <li><b>Tytuł</b>: Ray of sun</li>
-                       <li><b>Wykonawca</b>: Ray Charles</li>
-                       <li><b>Gatunek</b>: Pop</li>
-                       <li><b>Wybrane dla ciebie</b>: Tak</li>
+                       <li><b>Tytuł</b>: {this.state.title}</li>
+                       <li><b>Wykonawca</b>: {this.state.band}</li>
+                       <li><b>Gatunek</b>: {this.state.genre}</li>
+                       <li><b>Wybrane dla ciebie</b>: {this.state.recommended}</li>
                    </ul>
                </div>
                <div className="player">
-                   <audio controls preload="false">
-                        <source src="../../../../temp/power-juice.mp3" />
+                   <audio controls="controls">
+                        <source src={this.state.src} />
                    </audio>
                </div>
            </div>
