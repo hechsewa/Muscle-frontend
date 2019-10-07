67304ac4212cbc35226eb0302dba79c9be49abb3
Ewa Hechsman <ewa.hechsman@o2.pl>
2019-10-07 20:31:43 +0200

    build dir

---
+++ frontend/reactapi/build/precache-manifest.93f8a2aa4f55aab807e26e2080284d4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f18faaf4c634fabeb3b655a1d73e3c62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "514271cf131ab6a73133",
+    "url": "/static/css/main.08847c27.chunk.css"
+  },
+  {
+    "revision": "a95b010afc37b97e958a",
+    "url": "/static/js/2.0fcdd358.chunk.js"
+  },
+  {
+    "revision": "514271cf131ab6a73133",
+    "url": "/static/js/main.5742a978.chunk.js"
+  },
+  {
+    "revision": "a120cd6867c324a2f3bb",
+    "url": "/static/js/runtime-main.aaa78109.js"
+  },
+  {
+    "revision": "65c0da04a5499aa0e8fb94ccd8ddd544",
+    "url": "/static/media/01 All Of Me.65c0da04.mp3"
+  }
+]);
