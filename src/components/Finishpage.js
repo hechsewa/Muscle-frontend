d38766216da218cb023ebd9bcbf4336e6990a457
Ewa Hechsman <ewa.hechsman@o2.pl>
2019-12-04 16:41:42 +0100

    finishpage closes connection

--- src/components/Finishpage.js
+++ src/components/Finishpage.js
@@ -1,6 +1,18 @@
 import React from "react"
+import axios from "axios";
+
+function close() {
+    axios
+            .get('https://muscle-server.herokuapp.com/finish',
+                )
+            .then(response => {
+                }).catch((error) => {
+                console.log(error);
+        })
+}
 
 function Finishpage() {
+    close();
     return(
         <div className="finish">
             <h1>Koniec</h1>
